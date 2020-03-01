dcf8b267601b611b24e6f1a766d8e67845e134ec
ManasaGottimukkula <17wh1a05b6@bvrithyderabad.edu.in>
2020-03-01 14:08:37 +0530

    adding methods in classes

--- NewYearGifts/src/main/java/com/epam/NewYearGifts/Chocolates.java
+++ NewYearGifts/src/main/java/com/epam/NewYearGifts/Chocolates.java
@@ -1,5 +1,13 @@
 package com.epam.NewYearGifts;
 
 public abstract class Chocolates {
+	int cweight;
+	public void setCWeight(int weight) {
+		this.cweight = weight;
+	}
+	public void getCWeight(int weight) {
+		this.cweight = weight;
+	}
+	public abstract int calculateWeight(int quantity, int weight);
 
 }
--- NewYearGifts/src/main/java/com/epam/NewYearGifts/Sweets.java
+++ NewYearGifts/src/main/java/com/epam/NewYearGifts/Sweets.java
@@ -1,5 +1,13 @@
 package com.epam.NewYearGifts;
 
 public abstract class Sweets {
+	int sweight;
+	public void setSWeight(int weight) {
+		this.sweight = weight;
+	}
+	public void getSWeight(int weight) {
+		this.sweight = weight;
+	}
+	public abstract int calculateWeightS(int quantity, int weight);
 
 }
